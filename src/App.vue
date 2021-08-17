1e15d2a2cef5e4ff8b8956ef397cc37355839a35
tambing <tambingeras@gmail.com>
2021-08-17 17:06:38 +0700

    commit lesson 18 - 28

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1><br>
+  <input type="text" ref="name">
+  <button @click="handleClick">Click Me</button>
+  <br><br>
+  <button @click="toggleModal"  @keydown.esc="showModal = false">Show Modal</button>
+  <div v-if="showModal">
+    <Modal :header="header" content="Modal Content" theme="sale" @close="toggleModal"/>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+// import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
+// global css dapat di import dalam component yang akan menggunakannya atau di main.js
+// import './assets/css/global.css'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Modal
+  },
+  data(){
+    return {
+      title: 'My First Vue App',
+      header: ['Header Modal', 1],
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick(){
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+      this.$refs.name.value = 'oke'
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    }
+  },
+  
+
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
@@ -22,5 +54,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1{
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- @click."modifiers" bisa di gunakan untuk menentukan modifier klik seperti "shift, alt, ctrl, self dll" -->
+    <div class="backdrop" @click.self="clickBackdrop">
+        <div class="modal" :class="{sale : theme === 'sale'}">
+            <h1>{{ dataHead }}</h1>
+            <p>{{ content }}</p>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        props: ['header', 'content', 'theme'],
+        data(){
+            return{
+                dataHead: this.header
+            }
+        },
+        methods: {
+            clickBackdrop(){
+                this.$emit('close')
+            }
+        },
+        
+    }
+</script>
+<style scoped>
+    /* untuk component style bisa menambahkan "scoped" atau menggunakan nama 
+    class yang lebih detail untuk setiap element html */
+        /* // componet style */
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop{
+        top: 0;
+        /* left: 0; */
+        position: fixed;
+        background: rgba(0, 0, 0, 0.5);
+        width: 100%;
+        height: 100%;
+    }
+    h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    p{
+        font-style: normal;
+    }
+    .sale{
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1{
+        color: white;
+    }
+</style>
